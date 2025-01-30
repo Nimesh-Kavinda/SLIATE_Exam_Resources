028ee99c1d913922a2c84d334c35cf396fb4ce0a
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2025-01-30 12:41:49 +0530

    Add some GSAP to home page

--- script.js
+++ script.js
@@ -6,6 +6,41 @@ gsap.from('.btn-group a', {
     ease: 'elastic'
 });
 
+gsap.from('.pic_innertext', {
+    duration: 1,
+    x: -100,
+    opacity: 0,
+    stagger: 0.4,
+    ease: 'elastic'
+});
+
+
+gsap.fromTo(".welcome_section", 
+    { opacity: 0, y: 50, scale: 0.9 }, 
+    { opacity: 1, y: 0, scale: 1, duration: 1.5, ease: "power4.out",
+      scrollTrigger: {
+        trigger: ".welcome_section",
+        start: "top 80%",
+        toggleActions: "play none none none"
+      }
+    }
+);
+
+
+gsap.fromTo(".welcome_section span", 
+    { color: "#000", scale: 1 }, 
+    { color: "#007bff", scale: 1.2, duration: 1, ease: "elastic.out(1,0.3)", 
+      scrollTrigger: {
+        trigger: ".welcome_section",
+        start: "top 80%"
+      }
+    }
+);
+
+
+
+
+
 
 
 
